fff277bfde847f381a34a7be86202bf4268725df
ChaosCrider <ChaosCrider@hotmail.com>
2024-06-07 20:26:36 -0400

    github webhook -> jenkins configs

--- start_ctrlService.py
+++ start_ctrlService.py
@@ -2,8 +2,6 @@ import config as config
 from config import db, app
 from controller import beast_controller, ability_controller, plan_controller, user_controller
 
-print('CICD is the worse thin kto setup, it never tells you what goes wrong , Hwo am i supposed to getanything done with this uncooperative deisgn...')
-
 import utils.database_reset as db_restart
 
 db_restart.reset_db(db, app)
